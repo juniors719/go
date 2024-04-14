3aa33ec487fe42de2563809239c404887894d597
Júnior Silva <junior.silva@alu.ufc.br>
2024-04-14 15:59:28 -0300

    #

--- 2/main.go
+++ 2/main.go
@@ -2,12 +2,25 @@ package main
 
 const a = "Valor Constante"
 
-var b bool // declarando uma variável b do tipo booleana
+// escopo global
+var (
+	// declarando uma variável b do tipo booleana
+	// por padrão, o valor inferido é false
+	b bool
+
+	// valor padrão = 0
+	c int
+
+	// valor padrão = ""
+	d string
+
+	// valor padrão = +0.000000e+000
+	e float64
+
+	f bool = true // declarando e atribuindo
+)
 
 func main() {
-
-	// b = true
-
-	println(b)
-
+	a := "x" // short declaration
+	println(a)
 }
