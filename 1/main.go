babffcb920d3ee1d9c2e556f2469c501586fb53d
Júnior Silva <junior.silva@alu.ufc.br>
2024-04-14 11:12:31 -0300

    first commit

---
+++ 1/main.go
@@ -0,0 +1,8 @@
+// O pacote main é o ponto de entrada para o programa Go.
+// Ele contém a função main, que é executada automaticamente quando o programa inicia.
+// Neste pacote, estamos imprimindo a mensagem "Hello" no console.
+package main
+
+func main() {
+	println(a)
+}
---
+++ 2/main.go
@@ -0,0 +1,13 @@
+package main
+
+const a = "Valor Constante"
+
+var b bool // declarando uma variável b do tipo booleana
+
+func main() {
+
+	// b = true
+
+	println(b)
+
+}
